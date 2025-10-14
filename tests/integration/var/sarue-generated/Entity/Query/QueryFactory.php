d04a549f3a80e19dbc0adc27526d7fa70a7cbea6
José San Martin <jose@josesanmartin.net>
2025-10-14 12:37:21 -0300

    Removes generated code from repository

--- tests/integration/var/sarue-generated/Entity/Query/QueryFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Sarue\Orm\Tests\Integration\Dummy\Generated\Entity\Query;
-
-class QueryFactory extends \Sarue\Orm\Query\QueryFactoryBase {
-function getDummyEntityQuery(): \Sarue\Orm\Tests\Integration\Dummy\Generated\Entity\Query\DummyEntityQuery { return $this->instantiateQuery('Sarue\Orm\Tests\Integration\Dummy\Generated\Entity\Query\DummyEntityQuery'); }
-}

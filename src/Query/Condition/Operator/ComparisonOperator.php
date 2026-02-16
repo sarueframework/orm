82d352e1cf55990b37f1ab25ee5ce368b3773c35
José San Martin <jose@josesanmartin.net>
2026-02-16 17:29:26 -0300

    Fixes ComparisonOperator enum

--- src/Query/Condition/Operator/ComparisonOperator.php
+++ src/Query/Condition/Operator/ComparisonOperator.php
@@ -2,12 +2,12 @@
 
 namespace Sarue\Orm\Query\Condition\Operator;
 
-enum ComparisonOperator
+enum ComparisonOperator: string
 {
-    case GreaterThan;
-    case GreaterThanOrEqualTo;
-    case LessThan;
-    case LessThanOrEqualTo;
-    case EqualTo;
-    case NotEqualTo;
+    case GreaterThan = '>';
+    case GreaterThanOrEqualTo = '>=';
+    case LessThan = '<';
+    case LessThanOrEqualTo = '<=';
+    case EqualTo = '=';
+    case NotEqualTo = '<>';
 }
